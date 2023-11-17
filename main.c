536adfc3740f9745809f91a0e4a04415baecffb5
Rana-000 <147792326+Rana-000@users.noreply.github.com>
2023-11-17 14:03:09 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+    
+    printf("HelloWorl!gnimmargorP C ot emocleW\n");
+    printf("hello rana\n");
+
+    int x=10,y=2*x,z=x+y;
+
+    printf("x=%d\n",x);
+    printf("y=%d\n",y);
+    printf("z=%d\n",z);
+
+    int price=500;
+    double result=price*1.1;
+
+    printf("result=%7f\n",result);
+    return 0; 
+}
